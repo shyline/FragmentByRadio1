54fa7fbecb12277b1bcbf0d2d1fc6ee4e57078ab
wucui <wucui@zhixueyun.com>
2017-05-31 17:19:11 +0800

    111

---
+++ app/src/main/java/com/fragmentbyradio/main/MainActivity.java
@@ -0,0 +1,18 @@
+package com.fragmentbyradio.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import main.com.fragmentbyradio.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
