db4c8731a15c88f593c2407b7dacb0ee6d144abb
wucui <wucui@zhixueyun.com>
2017-06-01 17:03:27 +0800

    first commit

---
+++ fbyrlibrary/src/main/java/com/fbyrlibrary/main/SimpleRadioButton.java
@@ -0,0 +1,18 @@
+package com.fbyrlibrary.main;
+
+import android.content.Context;
+import android.widget.RadioButton;
+
+/**
+ * Created by john on 2017/5/31.
+ */
+
+public class SimpleRadioButton extends RadioButton{
+
+
+    public SimpleRadioButton(Context context) {
+        super(context);
+    }
+
+
+}
